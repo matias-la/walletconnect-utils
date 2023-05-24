2b532cf89816706f847874cfd4785ae72fb9984e
Matias Lang <matias.la@exodus.io>
2023-05-24 16:06:55 -0300

    fork @walletconnect/window-metadata

--- browser/window-metadata/test/index.test.ts
+++ browser/window-metadata/test/index.test.ts
@@ -1,7 +1,7 @@
 import "mocha";
 import * as chai from "chai";
 
-describe("@walletconnect/window-metadata", () => {
+describe("@exodus/walletconnect-window-metadata", () => {
   it("needs tests", () => {
     // needs tests
   });
